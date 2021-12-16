91973a06df2f0b0f564c0ceb66d7d64f03dce1fc
lgdbdumps <95185998+lgdbdumps@users.noreply.github.com>
2021-12-16 21:21:12 +0100

    Create extract_cid.py

---
+++ extract_cid.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+JSON_FILENAME = sys.argv[1]
+OUTPUT_FILENAME = sys.argv[2]
+INDEX = int(sys.argv[3])
+f = open(JSON_FILENAME)
+outf = open(OUTPUT_FILENAME, 'w')
+json_data = json.loads(f.read())
+l = list(json_data)
+nl = [el['cid'] + '\n' for el in l]
+outf.write(nl[INDEX])
+outf.close()
