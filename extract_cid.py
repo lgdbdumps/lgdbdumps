9f5588e49910c6afaf4f4cf6b365fbba9161741e
lgdbdumps <95185998+lgdbdumps@users.noreply.github.com>
2022-06-09 15:11:36 +0200

    Update extract_cid.py

--- extract_cid.py
+++ extract_cid.py
@@ -1,10 +1,14 @@
 import json
 import sys
 
+MAIN_CSV_OUTPUT = 'main_csv_output'
+COMPACT_CSV_OUTPUT = 'compact_csv_output'
+
 JSON_FILENAME = sys.argv[1]
 OUTPUT_FILENAME = sys.argv[2]
 f = open(JSON_FILENAME)
-outf = open(OUTPUT_FILENAME, 'w')
+outf = open(MAIN_CSV_OUTPUT, 'w')
+outf2 = open(COMPACT_CSV_OUTPUT, 'w')
 json_data = json.loads(f.read())
 dict_lst = list(json_data)
 length=len(dict_lst)
@@ -18,6 +22,9 @@ for i in range(length):
   else:
     name='libgen_compact_'
   fnames.append(name)
-for i in range(length): 
-  outf.write("%s,%s,%s,%s" % (fnames[i]+dates[i]+'.rar', sizes[i]+'M', dates[i], cids[i]))  
+for i in range(length):
+  if i % 2 == 0:
+    outf.write("%s,%s,%s,%s" % (fnames[i]+dates[i]+'.rar', sizes[i]+'M', dates[i], cids[i]))
+  else:
+    outf2.write("%s,%s,%s,%s" % (fnames[i]+dates[i]+'.rar', sizes[i]+'M', dates[i], cids[i]))
 outf.close()
