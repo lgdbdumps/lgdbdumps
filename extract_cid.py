d3a0ba1daf8ef5f57aa7c2937f6735f12e1a4930
lgdbdumps <95185998+lgdbdumps@users.noreply.github.com>
2022-06-09 15:04:15 +0200

    Update extract_cid.py

--- extract_cid.py
+++ extract_cid.py
@@ -3,11 +3,21 @@ import sys
 
 JSON_FILENAME = sys.argv[1]
 OUTPUT_FILENAME = sys.argv[2]
-INDEX = int(sys.argv[3])
 f = open(JSON_FILENAME)
 outf = open(OUTPUT_FILENAME, 'w')
 json_data = json.loads(f.read())
-l = list(json_data)
-nl = ['/ipfs/' + el['cid'] + '\n' for el in l]
-outf.write(nl[INDEX])
+dict_lst = list(json_data)
+length=len(dict_lst)
+fnames=[]
+cids = ['/ipfs/' + el['cid'] + '\n' for el in dict_lst]
+sizes = [ el['dagSize'] / (1024 * 1024) for el in dict_lst]
+dates = [el['created'].split('T')[0] for el in dict_lst]
+for i in range(length):
+  if i % 2 == 0:
+    name='libgen_'
+  else:
+    name='libgen_compact_'
+  fnames.append(name)
+for i in range(length): 
+  outf.write("%s,%s,%s,%s" % (fnames[i]+dates[i]+'.rar' sizes[i], dates[i], cids[i]))  
 outf.close()
